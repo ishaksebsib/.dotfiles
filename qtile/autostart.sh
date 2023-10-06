ef2dc55b0999fcb1b987f8eecad1256bcdb74466
ishaksebsib <ishaksebsib@gmail.com>
2023-10-06 18:38:26 +0300

    changes the autostart removed touchbad confs

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Apply wallpaper using wal
-# wal -b 282738 -i ~/Wallpaper/Aesthetic2.png &&
-
-# reverse the touch pad scrolling
-xinput set-prop 14 'libinput Natural Scrolling Enabled' 1 
-
-# Tocuh pad click enable
-xinput set-prop 14  "libinput Tapping Enabled" 1 
-
-# change the wall papaer
-# feh --bg-fill ./Wallpaper/background.jpg 
+# Apply WallPaper 
+feh --bg-fill ~/Wallpaper/background.jpg 
 
 #change the theme to dark mood 
-# darkman set dark 
+darkman set dark 
 
 # Start picom
 picom --config ~/.config/picom/picom.conf &
