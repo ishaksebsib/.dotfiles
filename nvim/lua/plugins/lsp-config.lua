cd2e27f6b7cdbc4981c1e050dd3402a1aa7e9c8f
ishaksebsib <ishaksebsib@gmail.com>
2024-02-23 22:17:27 +0300

    LSP and new telescope ui ADDED

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,51 @@
+local servers = {
+  "clangd",
+  "cssls",
+  "dockerls",
+  "gopls",
+  "html",
+  "jsonls",
+  "tsserver",
+  "lua_ls",
+  "pyright",
+  "rust_analyzer",
+  "tailwindcss",
+}
+
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup {
+			    ensure_installed = servers
+			}
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+
+ 		config = function()
+			local lspconfig = require("lspconfig")
+
+			for _, server in ipairs(servers) do
+			  lspconfig[server].setup({})
+			end
+
+			-- setup lsp for all language servers
+			-- See `:help vim.lsp.*` for documentation on any of the below functions
+			local opts = {}
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+			vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+		end
+	},
+}
