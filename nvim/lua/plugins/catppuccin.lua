43d72c79ec766f715e6aceb270d57b2f56da390b
ishaksebsib <ishaksebsib@gmail.com>
2024-02-23 20:28:43 +0300

    version 0.2 Done folder sturacute and lunaline added

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,4 +1,5 @@
-return {	"catppuccin/nvim", name = "catppuccin",
-			priority = 1000,
-			config = function() end
-		}
+return {
+	"catppuccin/nvim", name = "catppuccin",
+	priority = 1000,
+	config = function() end
+}
