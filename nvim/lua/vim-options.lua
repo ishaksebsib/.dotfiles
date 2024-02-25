968ee5da21b5c1898f8432e892d35e53e243c981
ishaksebsib <ishaksebsib@gmail.com>
2024-02-25 18:31:54 +0300

    color highligther and relative line enabled

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -11,8 +11,16 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- Set cliipBoard system's clipboard
 vim.cmd("set clipboard+=unnamedplus")
 
+-- enable color support in terminal
+vim.o.termguicolors = true
+
+-- Enable line numbers and set them to relative
+vim.o.number = true
+vim.o.relativenumber = true
+
 -- config tap
 vim.cmd("set noexpandtab") -- Use actual tabs instead of spaces
 vim.cmd("set tabstop=4") -- Number of spaces that a <Tab> in the file counts for
 vim.cmd("set softtabstop=4") -- Number of spaces to use when editing
 vim.cmd("set shiftwidth=4") -- Number of spaces used for autoindent
+
