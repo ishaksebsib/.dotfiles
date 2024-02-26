ccc36e036204346be763c3cd93e11c6ca2725cd6
ishaksebsib <ishaksebsib@gmail.com>
2024-02-26 19:43:25 +0300

    format on save added

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
@@ -11,6 +12,22 @@ return {
 				null_ls.builtins.formatting.black, -- formater for python
 				null_ls.builtins.diagnostics.eslint_d,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							-- vim.lsp.buf.formatting_sync() check ducumentation for more info
+
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
