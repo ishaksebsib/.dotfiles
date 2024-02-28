4d2e7e3d3aaea52c0f71b59b963ee316504f21a9
ishaksebsib <ishaksebsib@gmail.com>
2024-02-28 22:03:29 +0300

    commented out auto format on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -12,22 +12,24 @@ return {
 				null_ls.builtins.formatting.black, -- formater for python
 				null_ls.builtins.diagnostics.eslint_d,
 			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-							-- vim.lsp.buf.formatting_sync() check ducumentation for more info
+			-- uncomment if you want auto format when save
 
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
+			--[[on_attach = function(client, bufnr)]]
+			--[[if client.supports_method("textDocument/formatting") then]]
+			--[[vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })]]
+			--[[vim.api.nvim_create_autocmd("BufWritePre", {]]
+			--[[group = augroup,]]
+			--[[buffer = bufnr,]]
+			--[[callback = function()]]
+			--[[-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead]]
+			--[[-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead]]
+			--[[-- vim.lsp.buf.formatting_sync() check ducumentation for more info]]
+
+			--[[vim.lsp.buf.format({ async = false })]]
+			--[[end,]]
+			--[[})]]
+			--[[end]]
+			--[[end,]]
 		})
 
 		vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
