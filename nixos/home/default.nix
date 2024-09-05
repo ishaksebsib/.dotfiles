396778850978429beef726386566e3d5a17ca435
ishaksebsib <ishaksebsib@gmail.com>
2024-09-05 05:15:03 -0400

    [ENH] font config

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -22,6 +22,24 @@
 	   libreoffice
 	];
 
+	  
+	fonts = {
+		packages = with pkgs; [
+			noto-fonts
+			noto-fonts-emoji
+			monaspace
+			(nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+		];
+
+		fontconfig = {
+					defaultFonts = {
+						monospace = [ "Fira Code Nerd Font"];
+					};
+		};
+
+	};
+
+
 	home.stateVersion = "23.11";
 
 	programs.home-manager.enable = true;
