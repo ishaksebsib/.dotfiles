2ab4ac73cb26d63d30eb165efa3674d9b2233bee
ishaksebsib <ishaksebsib@gmail.com>
2024-10-23 23:28:37 +0300

    [ENH]: add stable branch

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,6 +2,7 @@
 	description = "ishak's nixos config";
 
 	inputs = {
+    stable.url = "github:nixos/nixpkgs/nixos-24.05";
 	  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
 	  home-manager = {
