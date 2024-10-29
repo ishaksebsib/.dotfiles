ef058d45701ed3126934d22354072f6d6ee0245f
ishaksebsib <ishaksebsib@gmail.com>
2024-10-29 19:24:18 +0300

    [CLN]: move qtile to nix home.file path

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -3,5 +3,22 @@
     ".config/nvim" = {
       source = ../dotfiles/nvim;
     };
+
+		# qtile configs
+		".config/qtile" = {
+			source = ../dotfiles/qtile/qtile;
+		};
+
+		".config/dunst" = {
+			source = ../dotfiles/qtile/dunst;
+		};
+
+		".config/rofi" = {
+			source = ../dotfiles/qtile/rofi;
+		};
+
+		".config/picom" = {
+			source = ../dotfiles/qtile/picom;
+		};
   };
 }
