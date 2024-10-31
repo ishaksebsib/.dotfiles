156ef72bfbf88c15ad9dd78ef837ff2093ecd78e
ishaksebsib <ishaksebsib@gmail.com>
2024-10-31 17:17:52 +0300

    [ENH]: Improve qtile config (codeformat and stracture)

--- dotfiles/qtile/qtile/config.py
+++ dotfiles/qtile/qtile/config.py
@@ -56,9 +56,15 @@ def move_app_to_next_screen():
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+
     # Switch between screens
     Key([mod], "0", lazy.next_screen(), desc="Move focus to next monitor"),
     Key([mod], "o", move_app_to_next_screen(), desc="Move current window to next monitor"),
+
+    # Toggle between fullscreen and windowed mode
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    Key([mod, "shift"], "f", lazy.hide_show_bar(), desc="Toggle show and hide toolbar/taskbar"), 
+
 
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -66,75 +72,61 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
-    Key([mod, "shift"], "f", lazy.hide_show_bar(), desc="Toggle show and hide toolbar/taskbar"), 
+
 
     # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+
+
+    # Grow windows.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
+
+
+    ## LAUNCHER 
+    Key([mod], "r", lazy.spawn('rofi -show drun'), desc="Launch app Launcher"),
+
+
+    # Open/Close Applications
+    Key([mod], "w", lazy.window.kill(), desc="Kill/close focused window"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([win], "e", lazy.spawn('thunar'), desc="Launch File Manager"),
+    Key([win], "w", lazy.spawn('google-chrome-stable'),desc='launch google chrome'),
+    Key([win], "t", lazy.spawn('telegram-desktop'), desc="Launch Telegram"),
+    Key([], "Print", lazy.spawn("rofi-screenshot"), desc="Launch Screenshot"),
+    Key([win], "c", lazy.spawn('code'), desc="Launch Visual Studio Code"),
+    Key(["control", "shift"], "Escape", lazy.spawn('WSysMon'), desc="Open task manager"),
+
+
+    # Open Power Off - Shutdown - Restart Options
+    Key([win], "l", lazy.spawn('i3lock -c 000000'), desc="Lock The Screen"),
+    Key([mod, "control"], "o", lazy.spawn('sh -c ~/.config/rofi/scripts/power'), desc="Open Power OFF Options"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    # Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
+
+    ## CONTROLERS
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s +4%"), desc="Increase brightness" ),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s 1%-"), desc="Decrease brightness"),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -q set Master 5%+"), desc="Increase volume"),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -q set Master 5%-"), desc="Decrease volume"),
+    Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle"), desc="Toggle Mute"), 
+    
+
+    # Toggle
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key(
         [mod, "shift"],
         "Return",
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-
-    # Run App
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill/close focused window"),
-    Key([win], "w", lazy.spawn('google-chrome-stable'),desc='launch google chrome'),
-    Key([win], "c", lazy.spawn('code'), desc="launch vscode"),
-    Key([win], "t", lazy.spawn('telegram-desktop'), desc="Open File Managere"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key(["control", "shift"], "Escape", lazy.spawn('WSysMon'), desc="Open task manager"),
-    # Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-
-    ## LAUNCHER 
-    Key([mod], "r", lazy.spawn('rofi -show drun'), desc="Launch app Launcher"),
-    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([win], "e", lazy.spawn('thunar'), desc="Open File Managere"),
-    # Open Power Off - Shutdown - Restart Options
-    Key([mod, "control"], "o", lazy.spawn('sh -c ~/.config/rofi/scripts/power'), desc="Open Power OFF Options"),
-    # Launch Lock Screen
-    Key([win], "l", lazy.spawn('i3lock -c 000000'), desc="Open Loack Screen"),
-    # Key([mod], "i", lazy.spawn('bash ~/.config/qtile/scripts/wifimenu.sh'), desc="Open Wifi Menu"),
-    # Launch Flameshot for screen shoot
-    Key([], "Print", lazy.spawn("rofi-screenshot"), desc="Take Screenshot"),
-
-
-    # CONTROAL BRIGHTNESS
-
-     # Increase brightness
-    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s +4%")),
-
-    # Decrease brightness
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s 1%-")),
-
-    # CONTROAL AUDIO 
-
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -q set Master 5%+")),#Increase volume
-
-
-    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -q set Master 5%-")),#Decrease volume
-
-    
-    Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),# Toggle Mute
-    
 ]
 
 # █▀▀ █▀█ █▀█ █░█ █▀█ █▀
@@ -144,8 +136,6 @@ keys = [
 group_labels = ["","","","","","","󰏃"]
 
 groups = [Group(f"{i+1}", label=group_labels[i]) for i in range(7)]
-
-
 
 for i in groups:
     keys.extend(
@@ -195,14 +185,12 @@ screens = [
                     background='#282738',
                 ),
 
-
                 widget.Image(
                     filename='~/.config/qtile/Assets/launch_Icon.png',
                     margin=2,
                     background='#282738',
                     mouse_callbacks={"Button1":power},
                 ),
-
 
                 widget.Image(
                     filename='~/.config/qtile/Assets/6.png',
@@ -414,7 +402,6 @@ floating_layout = layout.Floating(
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
 
 
 # AUTO Start Commnd this will only run on startapp
