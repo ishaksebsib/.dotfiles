7103f0b426bd530cffecb84064b6630b331db301
ishaksebsib <ishaksebsib@gmail.com>
2024-11-04 10:09:31 +0300

    [ENH][nvim] add markdown renderer

---
+++ dotfiles/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		require("render-markdown").setup({})
+	end,
+}
