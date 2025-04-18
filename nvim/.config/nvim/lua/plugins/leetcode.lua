2556427d1236c441d2c23ffc64b22e1528c1d471
ishaksebsib <ishaksebsib@gmail.com>
2025-04-18 21:19:50 +0300

    feat: add leetcode plugin for nvim

---
+++ nvim/.config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,14 @@
+return {
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html",   -- disable nvim-treesitter
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		-- "ibhagwan/fzf-lua",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+
+	config = function()
+		require("leetcode").setup({})
+	end
+}
