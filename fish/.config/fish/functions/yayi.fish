60b3205856e14b0892d5f401498dcb47a9675719
ishaksebsib <ishaksebsib@gmail.com>
2025-04-24 21:40:24 +0300

    feat: add fish alias to install packages from archlinux config using yayi

---
+++ fish/.config/fish/functions/yayi.fish
@@ -0,0 +1,4 @@
+function yayi --wraps='~/.dotfiles/archlinux/install_packages.sh' --description 'alias yayi=~/.dotfiles/archlinux/install_packages.sh'
+  ~/.dotfiles/archlinux/install_packages.sh $argv
+        
+end
