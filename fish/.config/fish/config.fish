70deef5e07307009caa87213515276e72f92c6a9
ishaksebsib <ishaksebsib@gmail.com>
2025-05-07 21:52:44 +0300

    feat: fish add pnpm setup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# setup pnpm
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+
 # set esc key to accept autosuggestion
 bind \e accept-autosuggestion
 
