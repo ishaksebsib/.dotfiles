87cec1df9defaf97888c82f295b93cff4f55fe7d
ishaksebsib <ishaksebsib@gmail.com>
2025-07-03 15:15:49 +0300

    feat: change yayi to parui

--- fish/.config/fish/functions/yayi.fish
+++
@@ -1,4 +0,0 @@
-function yayi --wraps='~/.dotfiles/archlinux/install_packages.sh' --description 'alias yayi=~/.dotfiles/archlinux/install_packages.sh'
-  ~/.dotfiles/archlinux/install_packages.sh $argv
-        
-end
