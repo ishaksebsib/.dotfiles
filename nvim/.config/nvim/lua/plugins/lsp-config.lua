2230f5426a42d2c06886c6a07144133e6237bf5e
ishaksebsib <ishaksebsib@gmail.com>
2025-08-01 15:58:26 +0300

    feat(nvim): add formatters to ensure install and add notification for formatter and lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,10 +1,10 @@
 function YankDiagnosticError()
 	vim.diagnostic.open_float()
 	vim.diagnostic.open_float()
-	local win_id = vim.fn.win_getid()   -- get the window ID of the floating window
-	vim.cmd("normal! j")                -- move down one row
-	vim.cmd("normal! VG")               -- select everything from that row down
-	vim.cmd("normal! y")                -- yank selected text
+	local win_id = vim.fn.win_getid() -- get the window ID of the floating window
+	vim.cmd("normal! j") -- move down one row
+	vim.cmd("normal! VG") -- select everything from that row down
+	vim.cmd("normal! y") -- yank selected text
 	vim.api.nvim_win_close(win_id, true) -- close the floating window by its ID
 end
 
@@ -20,29 +20,46 @@ local servers = {
 	rust_analyzer = {},
 	tailwindcss = {},
 	--eslint = {},
-	biome = {},
 	svelte = {},
 	elmls = {},
 	lua_ls = {
 		settings = {
 			Lua = {
 				diagnostics = {
-					globals = { 'vim' },
+					globals = { "vim" },
 				},
 				format = {
 					enable = false,
 				},
 			},
 		},
-	}
+	},
+}
+
+local formatters = {
+	biome = {},
+	stylua = {},
+	black = {},
 }
 
 return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			{ 'mason-org/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-			'WhoIsSethDaniel/mason-tool-installer.nvim',
+			{ "mason-org/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+
+			-- For lsp and formatting notifications
+			{
+				"j-hui/fidget.nvim",
+				opts = {
+					notification = {
+						window = {
+							winblend = 0, -- Background color opacity in the notification window
+						},
+					},
+				},
+			},
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			"williamboman/mason-lspconfig.nvim",
 			"neovim/nvim-lspconfig",
 			"hrsh7th/cmp-nvim-lsp",
@@ -50,15 +67,17 @@ return {
 
 		config = function()
 			--  Ensure the servers installed
-			require("mason-tool-installer").setup { ensure_installed = servers }
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, vim.tbl_keys(formatters or {}))
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			-- cmp_nvim lsp capabilities plus vim protocol capabilities
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 			-- Set up lsp for all servers with capabilities and configurations
 			for server, cfg in pairs(servers) do
-				cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
+				cfg.capabilities = vim.tbl_deep_extend("force", {}, capabilities, cfg.capabilities or {})
 				vim.lsp.config(server, cfg)
 				vim.lsp.enable(server)
 			end
@@ -71,20 +90,35 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
 			vim.keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, { desc = "Type definition" })
 
-
 			-- leader g fammily maps
 
-			vim.keymap.set({ "n", "v" }, "<leader>gca", vim.lsp.buf.code_action,
-				{ noremap = true, desc = "Code Actions" })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>gca",
+				vim.lsp.buf.code_action,
+				{ noremap = true, desc = "Code Actions" }
+			)
 
-			vim.keymap.set({ "n", "v" }, "<leader>gn", vim.lsp.buf.rename,
-				{ noremap = true, desc = "Rename All Same Variables" })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>gn",
+				vim.lsp.buf.rename,
+				{ noremap = true, desc = "Rename All Same Variables" }
+			)
 
-			vim.keymap.set("n", "<leader>ge", vim.diagnostic.open_float,
-				{ noremap = true, silent = true, desc = "Show current Error in float" })
+			vim.keymap.set(
+				"n",
+				"<leader>ge",
+				vim.diagnostic.open_float,
+				{ noremap = true, silent = true, desc = "Show current Error in float" }
+			)
 
-			vim.keymap.set('n', '<leader>gE', [[:lua YankDiagnosticError()<CR>]],
-				{ noremap = true, silent = true, desc = "Copy error" })
+			vim.keymap.set(
+				"n",
+				"<leader>gE",
+				[[:lua YankDiagnosticError()<CR>]],
+				{ noremap = true, silent = true, desc = "Copy error" }
+			)
 		end,
 	},
 }
