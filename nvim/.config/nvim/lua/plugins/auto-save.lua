3f62d3bed2bf1dff5c80170857aa88e2b23eecc8
ishaksebsib <ishaksebsib@gmail.com>
2025-08-11 19:47:21 +0300

    feat(nvim): remove tab and improve autosave

--- nvim/.config/nvim/lua/plugins/auto-save.lua
+++ nvim/.config/nvim/lua/plugins/auto-save.lua
@@ -8,8 +8,7 @@ return {
 		trigger_events = { -- See :h events
 			immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
 
-			-- NOTE: set this empty for harpoon menu to work
-			defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+			defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
 
 			cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
 		},
