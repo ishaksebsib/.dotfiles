80dc852bde1895c8b7480bfee6b74b5756b54889
ishaksebsib <ishaksebsib@gmail.com>
2025-12-04 10:54:19 +0300

    feat: add fisher, nvm and ghostty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,10 @@ set -gx PATH $PATH (go env GOPATH)/bin
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 
+# setup flyctl
+set -gx FLYCTL_INSTALL "$HOME/.fly"
+set -gx PATH "$FLYCTL_INSTALL/bin" $PATH
+
 # set esc key to accept autosuggestion
 bind \e accept-autosuggestion
 
@@ -33,3 +37,12 @@ zoxide init fish --cmd cd | source
 if test -f ~/.config/fish/.secrets.fish
     source  ~/.config/fish/.secrets.fish
 end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/pro/.opam/opam-init/init.fish' && source '/home/pro/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
