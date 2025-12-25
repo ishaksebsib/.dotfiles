2917ef4c4270f37a2cb27a9f7486e14b0626a2be
ishaksebsib <ishaksebsib@gmail.com>
2025-12-25 15:31:46 +0300

    feat(nvim): fff.nvim use prebuilt binary

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -1,18 +1,23 @@
-	return {
-		"dmtrKovalenko/fff.nvim",
-		build = "cargo build --release",
-		-- or if you are using nixos
-		-- build = "nix run .#release",
-		opts = {
-			-- pass here all the options
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = function()
+		-- this will download prebuild binary or try to use existing rustup toolchain to build from source
+		-- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+		require("fff.download").download_or_build_binary()
+	end,
+	--build = "cargo build --release",
+	-- or if you are using nixos
+	-- build = "nix run .#release",
+	opts = {
+		-- pass here all the options
+	},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fff").find_files()
+			end,
+			desc = "Open file picker",
 		},
-		keys = {
-			{
-				"<leader>ff",
-				function()
-					require("fff").find_files()
-				end,
-				desc = "Open file picker",
-			},
-		},
-	}
+	},
+}
