4ac6463a13898f1b0aa33950a0fb5eaa08c12302
ishaksebsib <ishaksebsib@gmail.com>
2025-12-31 19:46:17 +0300

    feat(nvim): add flash for flash jump

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,31 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+
+	---@type Flash.Config
+	opts = {
+		modes = {
+			search = {
+				enabled = true,
+			},
+		},
+	},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+	},
+}
