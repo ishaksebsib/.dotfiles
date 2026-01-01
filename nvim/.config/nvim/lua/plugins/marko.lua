a8ba352137d7cacc21d3118775b4b68a184a4c03
ishaksebsib <ishaksebsib@gmail.com>
2026-01-01 17:52:19 +0300

    fix(nvim): remove marko

--- nvim/.config/nvim/lua/plugins/marko.lua
+++ nvim/.config/nvim/lua/plugins/marko.lua
@@ -43,35 +43,35 @@ local function show_global_marks()
 end
 
 return {
-	"mohseenrm/marko.nvim",
-	config = function()
-		require("marko").setup()
-	end,
+	--"mohseenrm/marko.nvim",
+	--config = function()
+		--require("marko").setup()
+	--end,
 
-	-- Show global marks
-	vim.keymap.set("n", "<leader><leader>", show_global_marks, { desc = "Show global marks" }),
+	---- Show global marks
+	--vim.keymap.set("n", "<leader><leader>", show_global_marks, { desc = "Show global marks" }),
 
-	-- REVERSE the default go to marks
-	--
-	-- Goto global mark ' + a-z
-	vim.keymap.set("n", "'", function()
-		vim.cmd("normal! `" .. vim.fn.getcharstr():upper())
-	end, { desc = "Goto global mark <a-z>" }),
+	---- REVERSE the default go to marks
+	----
+	---- Goto global mark ' + a-z
+	--vim.keymap.set("n", "'", function()
+		--vim.cmd("normal! `" .. vim.fn.getcharstr():upper())
+	--end, { desc = "Goto global mark <a-z>" }),
 
-	-- Goto local mark leader + ' + a-z
-	vim.keymap.set("n", "<leader>'", function()
-		vim.cmd("normal! `" .. vim.fn.getcharstr())
-	end, { desc = "Goto local mark <a-z>" }),
+	---- Goto local mark leader + ' + a-z
+	--vim.keymap.set("n", "<leader>'", function()
+		--vim.cmd("normal! `" .. vim.fn.getcharstr())
+	--end, { desc = "Goto local mark <a-z>" }),
 
-	-- REVERSE the defualt set marks
-	--
-	-- Set global mark m + a-z
-	vim.keymap.set("n", "m", function()
-		vim.cmd("mark " .. vim.fn.getcharstr():upper())
-	end, { desc = "Set global mark <a-z>" }),
+	---- REVERSE the defualt set marks
+	----
+	---- Set global mark m + a-z
+	--vim.keymap.set("n", "m", function()
+		--vim.cmd("mark " .. vim.fn.getcharstr():upper())
+	--end, { desc = "Set global mark <a-z>" }),
 
-	-- Set local mark leader + m + a-z
-	vim.keymap.set("n", "<leader>m", function()
-		vim.cmd("mark " .. vim.fn.getcharstr())
-	end, { desc = "Set local mark <a-z>" }),
+	---- Set local mark leader + m + a-z
+	--vim.keymap.set("n", "<leader>m", function()
+		--vim.cmd("mark " .. vim.fn.getcharstr())
+	--end, { desc = "Set local mark <a-z>" }),
 }
