ea198a57925f1a7b6a5dccf6e95f8662b449cbb2
ishaksebsib <ishaksebsib@gmail.com>
2026-05-28 16:32:31 +0300

    feat(nix): add cli packages

--- home-manager/modules/programs/cli.nix
+++ home-manager/modules/programs/cli.nix
@@ -1,3 +1,28 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ curl ];
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # Network
+    curl
+    wget
+
+    # Power Management
+    tlp
+
+    # cli tools
+    brightnessctl
+    bluez
+    blueman
+    just
+    stow
+    less
+    lazygit
+    ripgrep
+    xclip
+    tree
+    ncdu
+    p7zip
+    wtype
+    unzip
+    zoxide
+  ];
 }
