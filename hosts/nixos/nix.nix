cf6edd9b39570fa1083fb84b2df89c32669206cf
Ishak Sebsib <ishaksebsib@gmail.com>
2026-05-30 19:57:29 +0300

    feat: add auto cleanup

--- hosts/nixos/nix.nix
+++ hosts/nixos/nix.nix
@@ -1,4 +1,9 @@
 { config, lib, ... }:
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 14d";
+  };
 }
