76935d28438e8a245c9b9916c64fe4cd72c5e54c
Ishak Sebsib <ishaksebsib@gmail.com>
2026-06-02 19:07:28 +0300

    locale: remove Amharic extraLocaleSettings, default all to en_US.UTF-8

--- hosts/nixos/locale.nix
+++ hosts/nixos/locale.nix
@@ -3,16 +3,4 @@
   time.timeZone = "Africa/Addis_Ababa";
 
   i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "am_ET";
-    LC_IDENTIFICATION = "am_ET";
-    LC_MEASUREMENT = "am_ET";
-    LC_MONETARY = "am_ET";
-    LC_NAME = "am_ET";
-    LC_NUMERIC = "am_ET";
-    LC_PAPER = "am_ET";
-    LC_TELEPHONE = "am_ET";
-    LC_TIME = "am_ET";
-  };
 }
