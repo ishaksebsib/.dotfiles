905fcba46747d2ebbb4a2792d6658ae2daa4b4d1
Ishak Sebsib <ishaksebsib@gmail.com>
2026-06-15 21:16:33 +0300

    feat: config herdr

--- hosts/nixos/users/pro/herdr.nix
+++ hosts/nixos/users/pro/herdr.nix
@@ -3,4 +3,14 @@
   home.packages = [
     inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.herdr
   ];
+
+  xdg.configFile."herdr/config.toml".text = ''
+    [terminal]
+    default_shell = "${pkgs.fish}/bin/fish"
+
+    [keys]
+    prefix = "super+a"
+    switch_tab = "ctrl+1..9"
+    switch_workspace = "prefix+1..9"
+  '';
 }
