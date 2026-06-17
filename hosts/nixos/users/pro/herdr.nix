b5937ad26d0290dcca5596da88742293aba3e925
Ishak Sebsib <ishaksebsib@gmail.com>
2026-06-17 10:37:28 +0300

    feat(herdr): enable system notifications

--- hosts/nixos/users/pro/herdr.nix
+++ hosts/nixos/users/pro/herdr.nix
@@ -2,6 +2,7 @@
 {
   home.packages = [
     inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.herdr
+    pkgs.libnotify
   ];
 
   xdg.configFile."herdr/config.toml".text = ''
@@ -15,5 +16,10 @@
         switch_tab = "ctrl+1..9"
         switch_workspace = "prefix+1..9"
         focus_agent = "super+ctrl+1..9"
+
+        [ui.toast]
+        enabled = true
+        delivery = "system"
+        delay_seconds = 1
   '';
 }
