c5d101095d7659e19fea23d2e233d0a9494a56d0
ishaksebsib <ishaksebsib@gmail.com>
2026-09-04 16:36:59 +0300

    feat: herdr + plannotator lite plugin

--- hosts/nixos/users/pro/herdr.nix
+++ hosts/nixos/users/pro/herdr.nix
@@ -2,25 +2,50 @@
 {
   home.packages = [
     inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.herdr
+    pkgs.bun
     pkgs.libnotify
   ];
 
   xdg.configFile."herdr/config.toml".text = ''
-    		onboarding = false
+    onboarding = false
 
-        [terminal]
-        default_shell = "${pkgs.fish}/bin/fish"
+    [terminal]
+    default_shell = "${pkgs.fish}/bin/fish"
 
-        [keys]
-        prefix = "super+a"
-        switch_tab = "ctrl+1..9"
-        switch_workspace = "prefix+1..9"
-        focus_agent = "super+ctrl+1..9"
-        remove_worktree = "prefix+shift+backspace"
+    [keys]
+    prefix = "super+a"
+    switch_tab = "ctrl+1..9"
+    switch_workspace = "prefix+1..9"
+    focus_agent = "super+ctrl+1..9"
+    remove_worktree = "prefix+shift+backspace"
 
-        [ui.toast]
-        enabled = true
-        delivery = "system"
-        delay_seconds = 1
+    [ui.toast]
+    enabled = true
+    delivery = "system"
+    delay_seconds = 1
+
+		# PLUGIN SETTINGS
+		
+
+		# PLANNOTATOR LITE PLUGIN
+		# https://github.com/plannotator/herdr-annotate
+
+    [[keys.command]]
+    key = "prefix+a"
+    type = "plugin_action"
+    command = "annotate.capture"
+    description = "annotate text"
+
+    [[keys.command]]
+    key = "prefix+shift+a"
+    type = "plugin_action"
+    command = "annotate.copy-context"
+    description = "copy annotations as context"
+
+    [[keys.command]]
+    key = "prefix+m"
+    type = "plugin_action"
+    command = "annotate.manage"
+    description = "manage annotations"
   '';
 }
